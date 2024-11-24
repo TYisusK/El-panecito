705af5bf1e85e1ac93d31888730e91e0b5908606
Juan Jesús Silvestre Chávez <134343358+TYisusK@users.noreply.github.com>
2024-11-23 21:46:36 -0600

    Update credenciales.js

--- middlewares/credenciales.js
+++ middlewares/credenciales.js
@@ -8,7 +8,7 @@ function verificarSesion(req, res, next) {
 
 function verificarAdmin(req, res, next) {
     console.log(req.session); // Verificar qué contiene la sesión
-    if (req.session && Number(req.session.rol) === 1) {
+    if (req.session (req.session.rol) == "1") {
         next();
     } else {
         res.render('templates/noAuto', { mensaje: 'Acceso no autorizado' });
